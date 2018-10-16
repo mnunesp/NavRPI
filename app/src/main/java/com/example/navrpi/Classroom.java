06d87d8784543f6ca8fe625536a440b09e13f0be
countdown25 <donotstealthis1>
2018-10-15 22:14:15 -0400

    removed non-null annotations

--- app/src/main/java/com/example/navrpi/Classroom.java
+++ app/src/main/java/com/example/navrpi/Classroom.java
@@ -2,11 +2,10 @@ package com.example.navrpi;
 
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
-import android.support.annotation.NonNull;
 
 @Entity
 public class Classroom {
-    @NonNull
+
     @PrimaryKey
     private String number = "113";
 
--- app/src/main/java/com/example/navrpi/Professor.java
+++ app/src/main/java/com/example/navrpi/Professor.java
@@ -2,11 +2,10 @@ package com.example.navrpi;
 
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
-import android.support.annotation.NonNull;
 
 @Entity
 public class Professor {
-    @NonNull
+
     @PrimaryKey
     private String firstName = "Dan";
 
