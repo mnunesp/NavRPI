897c83717656adf8e8e640a15945153ee21f1915
countdown25 <donotstealthis1>
2018-10-27 18:00:21 -0400

    fixed non null errors, added getter/setter methods

--- app/src/main/java/com/example/navrpi/Classroom.java
+++ app/src/main/java/com/example/navrpi/Classroom.java
@@ -2,12 +2,26 @@ package com.example.navrpi;
 
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
 
 @Entity
 public class Classroom {
 
+    @NonNull
     @PrimaryKey
     private String number = "113";
 
     private String building = "Darrin Communication Center";
+
+    public String getNumber() { return number; }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getBuilding() { return building; }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
