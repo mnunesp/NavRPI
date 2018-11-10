003ec25a956742f5fa5140f79af3c7ddf1862967
countdown25 <donotstealthis1>
2018-11-10 14:42:25 -0500

    changed buildings to single point

--- app/src/main/java/com/example/navrpi/BuildingAsyncPopulate.java
+++ app/src/main/java/com/example/navrpi/BuildingAsyncPopulate.java
@@ -13,16 +13,9 @@ public class BuildingAsyncPopulate extends AsyncTask<Void, Void, Void> {
     @Override
     protected Void doInBackground(final Void... params){
         bDao.deleteAll();
-        double[] n = new double[8];
-        n[0] = 42.73087 - .00025;
-        n[1] = 42.73087 + .0002;
-        n[2] = 42.73087 - .00025;
-        n[3] = 42.73087 + .0002;
-        n[4] = -73.682535 - .00025;
-        n[5] = -73.682535 - .00025;
-        n[6] = -73.682535 + .0002;
-        n[7] = -73.682535 + .0002;
-        Building walker = new Building("Walker", n);
+        double lat = 42.73087;
+        double lng = -73.682535;
+        Building walker = new Building("Walker", lat, lng);
         bDao.insert(walker);
         return null;
     }
